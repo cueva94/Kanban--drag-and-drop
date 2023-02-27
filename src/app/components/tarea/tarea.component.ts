d584334dda4648fe3ca92565ed3f840392a2ae03
Javier Herrera Cueva <javier.hcueva@babelgroup.com>
2023-02-27 14:00:51 +0100

    kanban

---
+++ src/app/components/tarea/tarea.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-tarea',
+  templateUrl: './tarea.component.html',
+  styleUrls: ['./tarea.component.scss']
+})
+export class TareaComponent {
+
+todo: string[] = []
+progress: string[]=[];
+done: string[]=[];
+
+tarea = '';
+
+
+drop(event: CdkDragDrop<string[]>) {
+  if (event.previousContainer === event.container) {
+    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+  } else {
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex,
+    );
+  }
+}
+
+agregarTarea(){
+  if(this.tarea === ''){
+    return
+  }
+  this.todo.push(this.tarea)
+  this.tarea = '';
+}
+eliminarTarea(){
+  alert('hola')
+}
+}
+console.log(onclick)
